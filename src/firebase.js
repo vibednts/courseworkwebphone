b2ec8683a36af4737549a5709cec37f12fec0e87
Denys <itsdenystsap@gmail.com>
2026-04-03 13:14:43 +0300

    API Vulnerability fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsv6cEOZX-82_-QRjjfqSypAdc5QuyJVM",
-  authDomain: "phonebook-c7aa8.firebaseapp.com",
-  projectId: "phonebook-c7aa8",
-  storageBucket: "phonebook-c7aa8.firebasestorage.app",
-  messagingSenderId: "106807621461",
-  appId: "1:106807621461:web:ca827d7d4be31ee9bfe1dc",
-  measurementId: "G-XNQX5Q4HD7"
+ apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
